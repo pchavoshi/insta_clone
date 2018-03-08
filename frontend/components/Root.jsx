4061a8ef903435217499176d3f408401b361c992
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-03-08 09:24:46 -0500

    heroku changes

--- frontend/components/Root.jsx
+++ frontend/components/Root.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Provider } from 'react-redux';
-import App from './app';
+import App from './App';
 import {HashRouter} from 'react-router-dom';
 
 const Root = ({store}) => (
