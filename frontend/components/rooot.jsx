70d3d477c4cc89ed9e87261e0c71ef0ae7bb8c57
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-03-08 09:53:23 -0500

    root rename

--- frontend/components/rooot.jsx
+++ frontend/components/rooot.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Provider } from 'react-redux';
-import app from './app';
+import App from './appp';
 import {HashRouter} from 'react-router-dom';
 
 const Root = (props) => (
--- frontend/shiba_clone.jsx
+++ frontend/shiba_clone.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { login, logout, signup } from './actions/session_actions';
-import Root from './components/root';
+import Root from './components/rooot';
 
 
 document.addEventListener("DOMContentLoaded", () => {
