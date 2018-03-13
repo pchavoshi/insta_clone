49ac2216b30fba94253f9949139c8e5803f9a197
Parisa Chavoshi <pchavoshi@gmail.com>
2018-03-13 15:52:09 -0400

    button workssssssss but page doesn't re-render accordingly

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -1,7 +1,6 @@
 class Api::FollowsController < ApplicationController
 
   def create
-    debugger
     follow = Follow.new(follow_params)
     if follow.save
       @user = User.find(follow.followed_id)
@@ -14,7 +13,8 @@ class Api::FollowsController < ApplicationController
   def destroy
     follow = Follow.find_by(follower_id: params[:follow][:follower_id], followed_id: params[:follow][:followed_id])
     follow.destroy
-    debugger
+    @user = User.find(params[:user_id])
+    render "api/users/show"
   end
 
 private
