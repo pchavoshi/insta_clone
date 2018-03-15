c7abd7f1a79416af6cce82f6f8d684f5d17d34a3
Parisa Chavoshi <pchavoshi@gmail.com>
2018-03-14 20:01:45 -0400

    lint user show

--- frontend/components/user_show.jsx
+++ frontend/components/user_show.jsx
@@ -1,47 +1,50 @@
 import React from 'react';
-import {FollowToggleContainer} from './follow_toggle_container';
+import { FollowToggleContainer } from './follow_toggle_container';
 
 class UserShow extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {followings: '', followers: '', posts: ''}
+    this.state = { followings: '', followers: '', posts: '' };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.props.getUser(this.props.match.params.userId);
   }
 
-  componentWillReceiveProps(nextProps){
+  componentWillReceiveProps(nextProps) {
     if (this.props.user.id != nextProps.match.params.userId) {
-        this.props.getUser(nextProps.match.params.userId);
+      this.props.getUser(nextProps.match.params.userId);
     } else if (this.state.followers != nextProps.user.followers.length) {
-      this.setState({followings: nextProps.user.followings.length,
+      this.setState({
+        followings: nextProps.user.followings.length,
         followers: nextProps.user.followers.length,
-        posts: nextProps.user.photo_ids.length});
+        posts: nextProps.user.photo_ids.length
+      });
     }
   }
 
-  render(){
+  render() {
     var follow_toggle;
-     if (this.props.currentUser.id != this.props.user.id) {
-      follow_toggle = < FollowToggleContainer user={this.props.user}/>;
-      }
+    if (this.props.currentUser.id != this.props.user.id) {
+      follow_toggle = <FollowToggleContainer user={this.props.user} />;
+    }
 
     const photos = Object.values(this.props.photos);
     const photo_array = photos.map(photo => photo.image);
 
-    return (<div>
-      <p>
-        <img src={`json.image_url asset_path(${this.props.user.image}(:original))`} />
-        {this.props.user.username}
-        {this.props.user.user_blurb}
-        following: {this.state.followings}
-        followers: {this.state.followers}
-        posts: {this.state.posts}
-      </p>
-      {follow_toggle}
-      {photo_array}
-    </div>);
+    return (
+      <div>
+        <p>
+          {this.props.user.username}
+          {this.props.user.user_blurb}
+          following: {this.state.followings}
+          followers: {this.state.followers}
+          posts: {this.state.posts}
+        </p>
+        {follow_toggle}
+        {photo_array}
+      </div>
+    );
   }
 }
 
