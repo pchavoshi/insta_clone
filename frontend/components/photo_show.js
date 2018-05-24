84019bae4a8f51280f7074beecea3c64f8029af2
parisa <pchavoshi@gmail.com>
2018-05-23 23:19:12 -0400

    change font and change comment overflow

--- frontend/components/photo_show.js
+++ frontend/components/photo_show.js
@@ -2,51 +2,42 @@ import React from 'react';
 import CommentIndexContainer from './comment_index_container';
 import UserHeader from './user_header';
 
-class PhotoShow extends React.Component {
-  // componentDidMount() {
-  //   if (this.props.modal !== 'notModal')
-  //     this.props.getPhoto(this.props.modal.id);
-  // }
-
-
-
-  render() {
+const PhotoShow = props => {
     return (
       <div  className="photo-show">
         <div className="container">
-          <img src={this.props.photo.image} className="photo" />
-          {this.props.modal === 'notModal' ? (
-            <label htmlFor={this.props.photo.id}>
+          <img src={props.photo.image} className="photo" />
+          {props.modal === 'notModal' ? (
+            <label htmlFor={props.photo.id}>
               <img src={window.comment} className="icons" />
             </label>
           ) : ('') }
         <div className="allComments">
           <div className="container">
-          {this.props.isModal === 'true' ? (
+          {props.isModal === 'true' ? (
           <div className="photo-bar">
-              <UserHeader user={this.props.photoUser}/>
+              <UserHeader user={props.photoUser}/>
               <hr/>
           </div>
           ) : (
             ''
           )}
           <div className="comment">
-            <span className="username">{this.props.photoUser.username}</span>
-            <span className="content">{this.props.photo.caption}</span>
+            <span className="username">{props.photoUser.username}</span>
+            <span className="content">{props.photo.caption}</span>
           </div>
           <CommentIndexContainer
-            comments={this.props.comments}
-            photoId={this.props.photo.id}
-            commentIndex={this.props.commentIndex}
-            indexItem={this.props.indexItem}
-            isModal={this.props.isModal}
+            comments={props.comments}
+            photoId={props.photo.id}
+            commentIndex={props.commentIndex}
+            indexItem={props.indexItem}
+            isModal={props.isModal}
           />
         </div>
         </div>
       </div>
       </div>
     );
-  }
 }
 
 export default PhotoShow;
