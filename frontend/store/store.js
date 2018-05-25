a5b1c070fa3130ac7d1fe89de2ab7c51e35cd4f1
parisa <pchavoshi@gmail.com>
2018-05-25 09:41:37 -0400

    change store to figure out heroku

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,16 +3,8 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import {createStore, applyMiddleware} from 'redux';
 
-let configureStore
-if (process.env.NODE_ENV !== 'production') {
-  configureStore = ( preloadedState = {} ) => {
-  return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger) )
-};
-
-} else {
-  configureStore = ( preloadedState = {} ) => {
-  return createStore(rootReducer, preloadedState, applyMiddleware(thunk) )
-};
-}
+const configureStore = ( preloadedState = {} ) => {
+   return createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger) )
+ };
 
 export default configureStore;
